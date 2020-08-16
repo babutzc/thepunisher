7493e057b30a3cceb7b82a9da76dc11a97ae11b6
Frank Spierings <frank@warpnet.nl>
2020-08-16 16:43:40 +0200

    basic 1st windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,1 +1,13 @@
+Add-Type -AssemblyName presentationCore
 
+# $DIR=split-path -Parent $MyInvocation.MyCommand.Definition
+$DIR=$pwd
+$AUDIOFILE=$("{0}\backdoor.mp3" -f $DIR)
+
+IEX "rundll32.exe user32.dll,LockWorkStation"
+
+
+$mp = New-Object System.Windows.Media.Mediaplayer
+$mp.Volume = 1
+$mp.Open($AUDIOFILE)
+$mp.Play()
