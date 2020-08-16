134a5f5ac0f8da3f7f4bf711b501f918e9423f0a
Frank Spierings <frank@warpnet.nl>
2020-08-16 16:19:49 +0200

    basic 1st osx.sh

--- osx.sh
+++ osx.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+AUDIOFILE="${DIR}/backdoor.mp3"
+
+osascript -e "set Volume 10"
+pmset displaysleepnow
+afplay -q 1 "${AUDIOFILE}"
